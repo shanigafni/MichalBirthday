618eaebe34fb407544e4d9016cd6d7840cf46621
Shani Gafni <shanigafni99@gmail.com>
2019-06-09 16:27:48 +0300

    Feat: build

---
+++ build/precache-manifest.5ca1a7a3a55f794de5e6b132a77f8930.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a326e09dcc858ed6eb0a63288c6588",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea6915f75f18b9833564",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "f6abe0455c24c2b247b3",
+    "url": "/static/css/main.54ca2537.chunk.css"
+  },
+  {
+    "revision": "ea6915f75f18b9833564",
+    "url": "/static/js/2.9e7041d6.chunk.js"
+  },
+  {
+    "revision": "f6abe0455c24c2b247b3",
+    "url": "/static/js/main.701b8bc7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a18447e2f059b011628410f775ed5315",
+    "url": "/static/media/cakes.a18447e2.jpg"
+  }
+]);
